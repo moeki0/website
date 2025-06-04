f05e3cff893419974e5969e146ab9406a93c6be0
KAWAKAMI Moeki <hi@moeki.org>
2025-06-05 00:17:31 +0900

    Update font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,6 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Inter Tight', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'sans-serif'],
-        serif: ['Lora', 'Georgia', 'serif']
-      },
       animation: {
         'fade': 'fade 0.5s ease-in-out'
       },
