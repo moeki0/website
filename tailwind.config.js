ad6605da894e0e1be9e6561a594285f6efa60f6e
KAWAKAMI Moeki <hi@moeki.org>
2025-06-05 00:21:49 +0900

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,6 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      animation: {
-        'fade': 'fade 0.5s ease-in-out'
-      },
-      keyframes: {
-        fade: {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '1' }
-        }
-      },
       typography: {
         DEFAULT: {
           css: {
